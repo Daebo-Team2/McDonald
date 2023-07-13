9dfbe90a3648a4486bb0f357289d0fd29d45f3bb
uhzoo0916 <uhzoo3366@gmail.com>
2023-07-13 18:02:47 +0900

    풀용 커밋

--- src/main/java/servlet/AdminServlet.java
+++ src/main/java/servlet/AdminServlet.java
@@ -2,17 +2,58 @@ package servlet;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import service.Action;
+import service.ActionForward;
+import service.PostAddService;
+import service.PostDetailService;
+import service.PostListService;
+import service.PostReplyService;
+
+
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
 @WebServlet(name = "AdminServlet", value = "/admin/*")
 public class AdminServlet extends HttpServlet {
-    @Override
+
+	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
+    	String requestURI = request.getRequestURI();
+    	String contextPath = request.getContextPath();
+    	String url_Command = requestURI.substring(contextPath.length());
+    	System.out.println(url_Command);
+    
+    	Action action = null;
+    	ActionForward forward = null;
+    	
+    	if(url_Command.equals("/admin/postpage.do")){	/*가맹점 문의 페이지 첫화면 */
+			/* action = new PostUpdateStatusService(); */
+    		action = new PostListService(); /*문의 내역 전체 조회 서비스 */
+    		forward = action.execute(request, response);  	   	
 
-    @Override
+    	}else if(url_Command.equals("/admin/postadd.do")){ /*문의글 작성 */    	    		
+    		action = new PostAddService();
+    		forward = action.execute(request, response);
+    	    	
+    	}else if(url_Command.equals("/admin/postmodal.do")){ /*문의글 상세 조회 (답변글 전체 확인) */
+    	    action = new PostDetailService();
+    		forward = action.execute(request, response);   	    		
+    	}
+    	
+    	
+    	if(forward != null) {	
+    		if(forward.isRedirect()) {	
+    			response.sendRedirect(forward.getPath());
+    		}else {	
+    			RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
+    			dis.forward(request,response);
+    		}
+    	}
+
+}
+	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    	this.doGet(request, response);
     }
 }
--- src/main/java/servlet/SuperServlet.java
+++ src/main/java/servlet/SuperServlet.java
@@ -2,6 +2,14 @@ package servlet;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import service.Action;
+import service.ActionForward;
+import service.PostAddService;
+import service.PostDetailService;
+import service.PostListService;
+import service.PostReplyService;
+
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
@@ -9,6 +17,39 @@ import java.io.IOException;
 public class SuperServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String requestURI = request.getRequestURI();
+    	String contextPath = request.getContextPath();
+    	String url_Command = requestURI.substring(contextPath.length());	
+    
+    	Action action = null;
+    	ActionForward forward = null;
+    	
+    	if(url_Command.equals("/super/postpage.do")){/*본사 문의페이지 첫 화면 */
+			/* action = new PostUpdateStatusService(); */
+    		action = new PostListService(); /*전체 조회 */ 
+    		forward = action.execute(request, response);  	   	
+
+    	}else if(url_Command.equals("/super/postreply.do")){ /*본사에 답글이 달리면 status 상태 2 업데이트 */
+    		action = new PostReplyService();
+    		forward = action.execute(request, response);
+    		
+    	}else if(url_Command.equals("/super/postmodal.do")){/*reno 가 null이 아닐때는 문의내역 조회 + 답변 작성  */
+    	    /*문의내역 상세보기 */	
+    		action = new PostDetailService();
+    		forward = action.execute(request, response);
+    	 
+    	}
+    	
+    	if(forward != null) {	
+    		if(forward.isRedirect()) {	
+    			response.sendRedirect(forward.getPath());
+    		}else {	
+    			RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
+    			dis.forward(request,response);
+    		}
+    	}
+
+    
     }
 
     @Override
