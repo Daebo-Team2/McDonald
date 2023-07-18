1c763cb911402126fd93df330c5ab7fab4610853
2ys2 <2ys2bau@gmail.com>
2023-07-18 16:51:50 +0900

    MenuAddService 유효성검사 추가, 이미지 저장 (임시)

--- src/main/java/service/MenuAddService.java
+++ src/main/java/service/MenuAddService.java
@@ -1,7 +1,13 @@
 package service;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import dao.MenuDAO;
 
@@ -12,17 +18,50 @@ public class MenuAddService implements Action {
 		ActionForward forward = new ActionForward();
 		MenuDAO dao = new MenuDAO();
 		
-		String category = request.getParameter("category");
+		String category = request.getParameter("category").trim();
 		String name = request.getParameter("name").trim();
 		int price = Integer.parseInt(request.getParameter("price").trim());
 		String[] foodnos = request.getParameterValues("foodno");
 		
 		int menuno = dao.addMenu(category, name, price);
+		
+		if  ( menuno ==0 ) {
+			return null;
+		}
+		
 		for (int i = 0; i < foodnos.length; i++ ) {
 		dao.addRecipe(menuno, Integer.parseInt(foodnos[i]) );
 		}
+		String fileName = null;
+		String extension = null;
 		
-		String image = "";
+		try {
+			Collection<Part> parts = request.getParts();
+			for(Part part : parts) {
+					if (part.getHeader("Content-Disposition").contains("filename=")) {
+						fileName = part.getHeader("Content-Disposition");
+						
+						if (part.getSize() > 0) {
+							
+							 String orifilename= part.getSubmittedFileName();
+							 extension = orifilename.substring(orifilename.lastIndexOf(".")+1,orifilename.length());
+							 fileName = Integer.toString(menuno);
+							 //System.out.println(orifilename);
+							 //System.out.println(fileName);
+							 //System.out.println(extension);
+							
+							File file = new File("C:/Users/KOSA/Pictures/Saved Pictures/" + fileName + "." + extension);
+							if (!file.exists()) {
+								file.createNewFile();
+							}
+							part.write("C:/Users/KOSA/Pictures/Saved Pictures/" + fileName + "." + extension);
+						}
+					}
+			}
+		} catch (IOException | ServletException e) {
+			e.printStackTrace();
+		}
+		String image = fileName + "." + extension;
 		dao.updateMenu(menuno, image);
 		
 		forward.setPath("/super/menuContent.do");
