1123e17b607dfb51ec0035538eb8be9a483add93
2ys2 <2ys2bau@gmail.com>
2023-07-18 16:50:45 +0900

    login.js id, pwd -> trim() 추가

--- src/main/WebContent/js/login.js
+++ src/main/WebContent/js/login.js
@@ -9,8 +9,8 @@ loginButton.addEventListener("click", (event) => {
 	/* a 태그나 submit 태그는 누르게 되면 href 를 통해 이동하거나, 창이 새로고침하여 실행됩니다.
 		preventDefault 를 통해 이러한 동작을 막아줄 수 있습니다. */
 	
-	const id = loginForm.id.value;
-	const pwd = loginForm.password.value;
+	const id = loginForm.id.value.trim();
+	const pwd = loginForm.password.value.trim();
 
 	if ( !id || id.trim().length == 0 ){
 		alert("아이디가 입력되지 않았습니다.");
