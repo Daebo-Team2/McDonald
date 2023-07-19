aed3da2bf586b3fe2f2a9d272a8cd4d740d782b2
kong <70005144+kongyb@users.noreply.github.com>
2023-07-19 17:24:57 +0900

    샘플 html삭제

--- src/main/WebContent/html/admin/order/order.js
+++
@@ -1,7 +0,0 @@
-// const delBtns = document.querySelectorAll("button.delBtn");
-
-// for (const btn of delBtns) {
-// 	btn.addEventListener("click", () => {
-// 		btn.parentElement.parentElement.parentElement.remove();
-// 	})
-// }
